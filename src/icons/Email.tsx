e88680e3e7cf78e10d6c523629b34c2b63c38b0a
vi.latyshev <storis2012@yandex.ru>
2021-07-24 16:44:40 +0300

    feat: Phone and Email icons move to top lvl

---
+++ src/icons/Email.tsx
@@ -0,0 +1,9 @@
+import { SvgIcon } from '@material-ui/core';
+
+import type { SvgIconProps } from '@material-ui/core';
+
+export const EmailIcon = (props: SvgIconProps) => (
+    <SvgIcon width="28" height="28" viewBox="0 0 28 28" {...props}>
+        <path d="M14 0a14 14 0 000 28h7v-2.8h-7C8 25.2 2.8 20.1 2.8 14 2.8 8 7.9 2.8 14 2.8c6 0 11.2 5.1 11.2 11.2v2c0 1.1-1 2.2-2.1 2.2S21 17.2 21 16v-2a7 7 0 10-2 5c.9 1.2 2.4 2 4.1 2a5 5 0 004.9-5v-2A14 14 0 0014 0zm0 18.2a4.2 4.2 0 110-8.4 4.2 4.2 0 010 8.4z" />
+    </SvgIcon>
+);
--- src/views/home/MainSection/TitleContactBlock/icons/Phone.tsx
+++
@@ -1,5 +0,0 @@
-export const PhoneIcon = (props: React.SVGProps<SVGSVGElement>) => (
-    <svg width="28" height="28" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-        <path fill="#2EC5CE" d="M27.2 3v-.1L21.3 0 15 8.4l2.9 3.9a6 6 0 01-5.6 5.6L8.4 15 0 21.3l2.9 5.8v.1a1.7 1.7 0 001.5.8h1.5A22.1 22.1 0 0028 5.9V4.4a1.7 1.7 0 00-.8-1.5zm-1.4 2.9c0 11-9 20-20 20H4.7l-1.9-4 5.7-4.2 3.2 2.4h.3a8.1 8.1 0 008.2-8.2v-.3l-2.4-3.2L22 2.7l3.8 2v1.2z" />
-    </svg>
-);
