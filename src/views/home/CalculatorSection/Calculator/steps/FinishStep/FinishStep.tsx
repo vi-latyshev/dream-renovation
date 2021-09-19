45f38c73c636d583b4bd991b5b2422a760e0f0e2
vi.latyshev <storis2012@yandex.ru>
2021-09-19 21:13:02 +0300

    feat: add Loading component

--- src/views/home/CalculatorSection/Calculator/steps/FinishStep/FinishStep.tsx
+++ src/views/home/CalculatorSection/Calculator/steps/FinishStep/FinishStep.tsx
@@ -6,6 +6,7 @@ import {
     makeStyles,
 } from '@material-ui/core';
 
+import { Loading } from 'components/Loading';
 import { PhoneContact } from 'components/PhoneContact';
 
 import { useCalculatorSteps } from '../../context/steps';
@@ -74,45 +75,49 @@ export const FinishStep = () => {
         getRelativeDays();
     }, [time]);
 
+    if (relativeDays === null) {
+        return <Loading />;
+    }
+
     return (
-        <div className={classes.finish}>
-            <Typography variant="h3" className={classes.title}>
-                Ваш расчёт:
-            </Typography>
-            <DataValueBlock label="Стоимость работ" data={`от ${price} руб/м2`} />
-            <DataValueBlock label="Черновые материалы" data={`от ${dirtyMaterialsPrice} руб/м2`} />
-            <DataValueBlock label="Чистовые материалы" data={`от ${clearlyMaterialsPrice} руб/м2`} />
-            <div className={classes.totalContainer}>
-                <Typography variant="h3">
-                    <Typography variant="h3" component="span" color="primary">
-                        Итого:
-                    </Typography>{' '}
-                    от {price + dirtyMaterialsPrice + clearlyMaterialsPrice} руб/м2
+        <Fade in>
+            <div className={classes.finish}>
+                <Typography variant="h3" className={classes.title}>
+                    Ваш расчёт:
                 </Typography>
-                <Fade in={relativeDays !== null}>
+                <DataValueBlock label="Стоимость работ" data={`от ${price} руб/м2`} />
+                <DataValueBlock label="Черновые материалы" data={`от ${dirtyMaterialsPrice} руб/м2`} />
+                <DataValueBlock label="Чистовые материалы" data={`от ${clearlyMaterialsPrice} руб/м2`} />
+                <div className={classes.totalContainer}>
+                    <Typography variant="h3">
+                        <Typography variant="h3" component="span" color="primary">
+                            Итого:
+                        </Typography>{' '}
+                        от {price + dirtyMaterialsPrice + clearlyMaterialsPrice} руб/м2
+                    </Typography>
                     <DataValueBlock
                         label="Срок ремонта"
                         data={`~ ${relativeDays}`}
                         classNames={classes.totalTime}
                     />
-                </Fade>
-            </div>
-            <div className={classes.footer}>
-                <div className={classes.contactQuestions}>
-                    <Typography variant="h3">
-                        Остались вопросы?
-                    </Typography>
-                    <PhoneContact />
                 </div>
-                <Button
-                    color="primary"
-                    variant="contained"
-                    onClick={restartSteps}
-                    className={classes.stepButton}
-                >
-                    Пересчитать
-                </Button>
+                <div className={classes.footer}>
+                    <div className={classes.contactQuestions}>
+                        <Typography variant="h3">
+                            Остались вопросы?
+                        </Typography>
+                        <PhoneContact />
+                    </div>
+                    <Button
+                        color="primary"
+                        variant="contained"
+                        onClick={restartSteps}
+                        className={classes.stepButton}
+                    >
+                        Пересчитать
+                    </Button>
+                </div>
             </div>
-        </div>
+        </Fade>
     );
 };
