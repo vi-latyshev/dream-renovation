0a136fc1cd23343f3ffc328175e5b1c15045c403
vi.latyshev <storis2012@yandex.ru>
2021-10-26 23:46:12 +0300

    chore: upgrade next.js to 12

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ const nextConfig = {
             'src',
         ],
     },
+    images: {
+        formats: ['image/avif', 'image/webp'],
+    },
+    /* @TODO remove after enabling as default in next.js */
+    swcMinify: true,
 };
 
 module.exports = nextConfig;
