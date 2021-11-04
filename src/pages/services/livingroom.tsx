287468c7ca6898f7a72d47e7bcda8d51b6bf5368
vi.latyshev <storis2012@yandex.ru>
2021-11-05 01:47:39 +0300

    feat: start livingroom page in /services

---
+++ src/pages/services/livingroom.tsx
@@ -0,0 +1,16 @@
+import { PageBase } from 'components/Page';
+
+import type { PageMetaProps } from 'components/Page';
+
+const meta: PageMetaProps = {
+    title: 'Гостиная, спальня, детская',
+    description: '',
+};
+
+const LivingRoom = () => (
+    <PageBase meta={meta}>
+        <></>
+    </PageBase>
+);
+
+export default LivingRoom;
