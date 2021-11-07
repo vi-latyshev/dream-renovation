f81dc009689a684b18a22b3ea5f268bcea53a75b
vi.latyshev <storis2012@yandex.ru>
2021-11-07 05:02:14 +0300

    feat: add steps section

---
+++ src/views/home/StepSection/StepSection.tsx
@@ -0,0 +1,76 @@
+import { Grid, makeStyles, Typography } from '@material-ui/core';
+
+import { SectionWithContainer } from 'components/Section';
+
+import { BigRulerIcon } from './icons/BigRuler';
+import { StepItem } from './components/StepItem';
+
+const useStyles = makeStyles(({ spacing }) => ({
+    section: {},
+    stepsContainer: {
+        margin: spacing(8, 0),
+    },
+    ruleIcon: {
+        width: 1420,
+        height: 90,
+    },
+}));
+
+export const StepSection = () => {
+    const classes = useStyles();
+
+    return (
+        <SectionWithContainer maxWidth="xl">
+            <Typography variant="h1">
+                Пройдём следующие этапы:
+            </Typography>
+            <Grid
+                container
+                direction="column"
+                className={classes.stepsContainer}
+            >
+                <Grid
+                    item
+                    container
+                    spacing={10}
+                    direction="row"
+                    justifyContent="flex-start"
+                >
+                    <StepItem step="1" title="Заявка на услуги">
+                        Перед началом работы вы оставляете нам заявку по форме ниже
+                        либо набрав нам по телефону.
+                    </StepItem>
+                    <StepItem step="3" title="Разработка проекта">
+                        После составления сметы вместе с дизайнером составляется
+                        проект по вашему собственному ТЗ либо на заказ по вашим предпочтениям.
+                    </StepItem>
+                    <StepItem step="5" title="Ремонтные работы">
+                        Оформление потолков, отделка поверхностей, монтаж межкомнатных
+                        дверей, освещение и остальные типа работ.
+                    </StepItem>
+                </Grid>
+                <Grid
+                    item
+                    container
+                    spacing={10}
+                    direction="row"
+                    justifyContent="flex-end"
+                >
+                    <StepItem step="2" title="Выезд замерщика">
+                        После подтверждения заявки по адресу выезжает наш замерщик
+                        для уточнения всевозможных деталей и составления сметы.
+                    </StepItem>
+                    <StepItem step="4" title="Подготовительные работы">
+                        После разработки  наши сотрудники займутся подготовкой объекта
+                        к работам, Закупкой материалов, демонтажем старой отделки и пр.
+                    </StepItem>
+                    <StepItem step="6" title="Сдача объекта">
+                        Бригада займётся уборкой помещений, выносом мусора, финишной
+                        отделочной работой, проведут работу с краской и декоративными элементами.
+                    </StepItem>
+                </Grid>
+            </Grid>
+            <BigRulerIcon color="secondary" className={classes.ruleIcon} />
+        </SectionWithContainer>
+    );
+};
--- src/views/home/index.ts
+++ src/views/home/index.ts
@@ -1,5 +1,6 @@
 export * from './MainSection';
 export * from './ServicesSection';
 export * from './AboutCompanySection';
+export * from './StepSection';
 export * from './CalculatorSection';
 export * from './ContactUsSection';
