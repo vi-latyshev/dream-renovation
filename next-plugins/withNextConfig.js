09492ac80ad61401b9de05419bb6295ad7f05917
vi.latyshev <storis2012@yandex.ru>
2022-03-12 16:22:44 +0300

    feat: next config to withNextConfig [WIP]]

---
+++ next-plugins/withNextConfig.js
@@ -0,0 +1,63 @@
+/**
+ * Add environment variables to all app
+ *
+ * NOTE: Destructure process.env variables won't work due to the nature of webpack DefinePlugin
+ *
+ * @type {import('./plugins').NextPluginType}
+ */
+module.exports = (nextConfig, _nextComposePlugins = {}) => ({
+    // reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: '/services',
+                destination: '/#services',
+                permanent: true,
+            },
+        ];
+    },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'X-DNS-Prefetch-Control',
+                        value: 'on',
+                    },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=63072000; includeSubDomains; preload',
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block',
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'strict-origin',
+                    },
+                ],
+            },
+        ];
+    },
+    poweredByHeader: false,
+    eslint: {
+        dirs: [
+            'src',
+        ],
+    },
+    images: {
+        formats: ['image/avif', 'image/webp'],
+    },
+    /* @TODO remove after enabling as default in next.js */
+    swcMinify: true,
+});
--- next.config.js
+++ next.config.js
@@ -1,72 +1,9 @@
 const withPlugins = require('next-compose-plugins');
 
-const IS_PRODUCTION = process.env.VERCEL_ENV === 'production';
-const DOMAIN = process.env.NEXT_PUBLIC_URL;
-
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    // reactStrictMode: true,
-    async redirects() {
-        return [
-            {
-                source: '/services',
-                destination: '/#services',
-                permanent: true,
-            },
-        ];
-    },
-    async headers() {
-        return [
-            {
-                source: '/:path*',
-                headers: [
-                    {
-                        key: 'X-DNS-Prefetch-Control',
-                        value: 'on',
-                    },
-                    {
-                        key: 'Strict-Transport-Security',
-                        value: 'max-age=63072000; includeSubDomains; preload',
-                    },
-                    {
-                        key: 'X-XSS-Protection',
-                        value: '1; mode=block',
-                    },
-                    {
-                        key: 'X-Frame-Options',
-                        value: 'DENY',
-                    },
-                    {
-                        key: 'X-Content-Type-Options',
-                        value: 'nosniff',
-                    },
-                    {
-                        key: 'Referrer-Policy',
-                        value: 'strict-origin',
-                    },
-                ],
-            },
-        ];
-    },
-    poweredByHeader: false,
-    env: {
-        IS_PRODUCTION,
-        DOMAIN,
-    },
-    eslint: {
-        dirs: [
-            'src',
-        ],
-    },
-    images: {
-        formats: ['image/avif', 'image/webp'],
-    },
-    /* @TODO remove after enabling as default in next.js */
-    swcMinify: true,
-};
+const withNextConfig = require('./next-plugins/withNextConfig');
+const withEnv = require('./next-plugins/withEnv');
 
 module.exports = withPlugins([
-
-], nextConfig);
+    withNextConfig,
+    withEnv,
+]);
