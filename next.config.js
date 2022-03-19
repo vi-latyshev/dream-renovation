e8c84b019579e462612ad4dcbece7ba7884033b6
vi.latyshev <storis2012@yandex.ru>
2022-03-19 23:41:29 +0300

    feat: move dev-precompile script to withDevPrecompile

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 const withPlugins = require('next-compose-plugins');
+const {
+    PHASE_DEVELOPMENT_SERVER,
+} = require('next/constants');
 
 const withNextConfig = require('./next-plugins/withNextConfig');
 const withEnv = require('./next-plugins/withEnv');
+const withDevPrecompile = require('./next-plugins/withDevPrecompile');
 
 module.exports = withPlugins([
     withNextConfig,
     withEnv,
+    [withDevPrecompile, [PHASE_DEVELOPMENT_SERVER]],
 ]);
